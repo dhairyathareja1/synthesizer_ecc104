0c68cd98dd09d420e9169d5606b3be12f18afaa5
Dhairya <dhairyathareja1@gmail.com>
2026-04-30 20:41:59 +0530

    feat: add more expressions and fixed indent at some places

--- synth.py
+++ synth.py
@@ -102,8 +102,8 @@ class Synthesizer:
         output = left.strip()
         right = right.strip()
 
-#a.b+c
-        if "." in right and "+" in right:
+        #a.b+c
+        if "." in right and "+" in right and right.count(".")==1:
             part1, part2 = right.split("+")
             a, b = part1.split(".")
             c = part2.strip()
@@ -117,8 +117,8 @@ class Synthesizer:
             gate2 = OrGate(temp, c, output)
             gate2.draw()
 
-#a+b.c
-        elif "+" in right and "." in right:
+        #a+b.c
+        elif "+" in right and "." in right and right.count(".")==1:
             part1, part2 = right.split("+")
             b, c = part2.split(".")
             a = part1.strip()
@@ -132,7 +132,7 @@ class Synthesizer:
             gate2 = OrGate(a, temp, output)
             gate2.draw()
 
-#a.b.c
+        #a.b.c
         elif right.count(".") == 2 and "+" not in right:
             a,b,c = right.split(".")
             temp = "x1"
@@ -146,7 +146,7 @@ class Synthesizer:
             gate2 = AndGate(temp, c.strip(), output)
             gate2.draw()
 
-#a+b+c
+        #a+b+c
         elif right.count("+") == 2 and "." not in right:
             a,b,c = right.split("+")
             temp= "x1"
@@ -160,7 +160,62 @@ class Synthesizer:
             gate2 = OrGate(temp, c.strip(), output)
             gate2.draw()
 
+        #a.b+b.c+c.a
+        elif right.count("+") ==2 and right.count(".") ==3:
+            part1, part2, part3 = right.split("+")
+
+            a,b = part1.split(".")
+            b,c = part2.split(".")
+            c,a = part3.split(".")
+
+            temp1 = "x1"
+            temp2 = "x2"
+            temp3 = "x3"
+            temp4 = "x4"
+
+            print("\nBoolean Expression :", code)
+            print("Synthesized Circuit :\n")
+
+            gate1 = AndGate(a.strip(), b.strip(), temp1)
+            gate1.draw()
+
+            gate2 = AndGate(b.strip(), c.strip(), temp2)
+            gate2.draw()
+
+            gate3 = AndGate(c.strip(), a.strip(), temp3)
+            gate3.draw()
+
+            gate4 = OrGate(temp1, temp2, temp4)
+            gate4.draw()
+
+            gate5 = OrGate(temp4, temp3, output)
+            gate5.draw()
+
+        #xor gate
+        elif " xor " in right or right == "ab'+a'b":
+
+            temp1 = "x1"
+            temp2 = "x2"
+            nota = "na"
+            notb = "nb"
+
+            print("\nXOR Circuit:\n")
+
+            gate1 = NotGate("a", nota)
+            gate2 = NotGate("b", notb)
+
+            gate3 = AndGate("a", notb, temp1)
+            gate4 = AndGate(nota, "b", temp2)
+
+            gate5 = OrGate(temp1, temp2, output)
+
+            gate1.draw()
+            gate2.draw()
+            gate3.draw()
+            gate4.draw()
+            gate5.draw()
+
 obj = Synthesizer()
 
-obj.gate_synthesize("y = not b")
-obj.boolean_synthesize("y = a.b+c")
+#obj.gate_synthesize("y = not b")
+obj.boolean_synthesize("y = a xor b")
